13156ace46fdb47fbfd5cb5e6c536b0bd9d7cc2f
苍旻白轮 <55022033+SomiaWhiteRing@users.noreply.github.com>
2026-03-13 23:43:46 +0800

    Update SupportButton.tsx

--- components/SupportButton.tsx
+++ components/SupportButton.tsx
@@ -17,6 +17,9 @@ const donationAcknowledgements: Array<{
   amount: string;
   message: string;
 }> = [
+  { date: "2026-03-13", name: "孔睦瑞", amount: "1", message: "云吸猫" },
+  { date: "2026-03-13", name: "yoj1pR", amount: "10", message: "辛苦了——" },
+  { date: "2026-03-13", name: "匿名", amount: "3", message: "音乐很多小众专辑缺失，后续可以支持让用户自行添加图片和文字" },
   { date: "2026-03-13", name: "官官", amount: "3", message: "阿巴阿巴阿巴" },
   { date: "2026-03-13", name: "匿名", amount: "1", message: "" },
   { date: "2026-03-13", name: "r1chard", amount: "10", message: "能不能出一个构成我的9本本子（（开发者注：商业本的话已经有了（小声）" },
